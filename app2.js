90e1865c8db34897f2e5512f2ed971f78ca6dd81
omarsaade <49005530+omarsaade@users.noreply.github.com>
2022-11-18 11:05:02 -0800

    Delete app2.js

--- app2.js
+++
@@ -1,8 +0,0 @@
-//built in module
-const os = require("os");
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log("Total Memory " + totalMemory);
-console.log("free Memory " + freeMemory);
